e88bacd75cd32a2bfc87d0fcc25a63752af32213
Louisss-Lam <louislam617@gmail.com>
2022-09-13 18:25:40 +0100

    Pomodoro-Timer

---
+++ src/components/Timer.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import PasueButton from './PasueButton';
+import PlayButton from './PlayButton';
+import SettingsButton from './SettingsButton';
+import { useContext, useState, useEffect, useRef } from 'react';
+import SettingsContext from './SettingsContext';
+
+const Timer = () => {
+    // const percentage = 66;
+    const red = '#f54e4e';
+    const green = '#4aec8c';
+
+    const settingsInfo = useContext(SettingsContext);
+    const [isPaused, setIsPaused] = useState(true);
+    const [mode, setMode] = useState('work');
+    const [secondsLeft, setSecondsLeft] = useState(0);
+
+    const secondsLeftRef = useRef(secondsLeft);
+    const isPausedRef = useRef(isPaused);
+    const modeRef = useRef(mode);
+
+    function switchMode() {
+        const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+        const nextSeconds = (nextMode === 'work' ? settingsInfo.workMinutes : settingsInfo.breakMinutes) * 60;
+
+        setMode(nextMode);
+        modeRef.current = nextMode;
+
+        setSecondsLeft(nextSeconds);
+        secondsLeftRef.current = nextSeconds;
+    }
+
+    function tick() {
+        secondsLeftRef.current--;
+        setSecondsLeft(secondsLeftRef.current);
+    }
+
+    function initTimer() {
+        setSecondsLeft(settingsInfo.workMinutes * 60);
+    }
+
+    useEffect(() => {
+        initTimer();
+
+        const interval = setInterval(() => {
+            if(isPausedRef.current) {
+                return;
+            }
+            if(secondsLeftRef.current === 0) {
+                return switchMode();
+            }
+            tick();
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [settingsInfo]);
+
+    const totalSeconds = mode === 'work' 
+        ? settingsInfo.workMinutes * 60 
+        : settingsInfo.breakMinutes * 60;
+    const percentage = Math.round(secondsLeft / totalSeconds) * 100;
+
+    const minutes = Math.floor(secondsLeft / 60);
+    let seconds = secondsLeft % 60;
+    if(seconds < 10) seconds = '0' + seconds;
+
+  return (
+    <div>
+        <CircularProgressbar 
+            value={percentage} 
+            text={minutes + ':' + seconds} 
+            styles={buildStyles({
+                textColor:'#fff',
+                pathColor: red,
+                tailColor:'rgba(255, 255, 255, .2)',
+            })}
+        />
+        <div style={{marginTop: '20px'}}>
+            {isPaused ? <PlayButton /> : <PasueButton />}
+        </div>
+        <div style={{marginTop: '20px'}}>
+            <SettingsButton onClick={() => {settingsInfo.setShowSettings(true)}} />
+        </div>
+    </div>
+  )
+}
+
+export default Timer;
